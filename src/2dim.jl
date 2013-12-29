298391ded692f119a04f573d3818a525bda1f927
natj <nattila.joonas@gmail.com>
2013-12-29 18:08:04 +0200

    general 2d CA

--- src/2dim.jl
+++ src/2dim.jl
@@ -1,7 +1,6 @@
 #2-dim CAs
 
 #Two dimensional Cellular Automaton
-
 #TODO: Change naming to be consistent
 type CA2d
 
@@ -12,12 +11,46 @@ type CA2d
     #Internal values
     cells::Array{Int8, 3}
 
-    function CA2d(rule::Rational{Int}, init::Array{Int,2}, gen::Int, k::Int=2, r::Int=1)
+    function CA2d(B::Array{Int,1}, 
+                  S::Array{Int,1}, 
+                  init::Array{Int,2}, 
+                  gen::Int, 
+                  k::Int=2, 
+                  r::Int=1)
 
-        B = digits(num(rule)) #born
-        S = digits(den(rule)) #survive
+        h, w = size(init)
+        cells = Array(Int8, h, w, gen)
+        cells[:, :, 1] = int8(init[:, :])
 
+        for g = 2:gen
+            for i = 1:h, j = 1:w
+                cc = -cells[i, j, g-1]
+                for p = (i-r):(i+r), q = (j-r):(j+r) 
 
+                    #Cyclic boundary conditions
+                    if p < 1; p = h-p; end
+                    if p > h; p = p-h; end
+                    if q < 1; q = w-q; end
+                    if q > w; q = q-w; end
 
+                    cc += cells[p, q, g-1]
+                end
+                cells[i, j, g] = eval_rule(cc, cells[i, j, g-1], B, S)
+            end #hw ij
+        end #gen
+
+        new(k, r, cells)
     end
 end
+
+#Evaluate life rules
+#TODO: make this more general
+function eval_rule(cc, olds, B, S, k=2)
+    if cc in B || (olds == 1 && cc in S)
+        return 1
+    end
+
+    return 0
+end
+
+
--- src/CellularAutomata.jl
+++ src/CellularAutomata.jl
@@ -3,8 +3,10 @@ module CellularAutomata
 import Base.show
 
 include("1dim.jl")
+include("2dim.jl")
 
 export CellularAutomaton,
+       CA2d,
        rule,
        show
 
@@ -39,6 +41,36 @@ function show(io::IO, ca::CellularAutomaton)
     print(io, bytestring(io2))
 end
 
+#Print CA to screen
+function show(io::IO, ca::CA2d) 
+    io2 = IOBuffer()
+
+    arr = ca.cells
+    h, w, gen = size(arr)
+
+    print(io2, "$(w)x$(h)x$(gen) Cellular Automaton")
+    print(io2, "\n")
+
+    #TODO: print rule
+
+    for g in 1:gen
+        for i = 1:h
+            print(io2,"\t")
+            for j = 1:w
+                if arr[i,j, g] == 0
+                    print(io2," ")
+                elseif arr[i,j, g] == 1
+                    print(io2, "#")
+                end
+            end
+            print(io2,"\n")
+        end
+        print(io2,"\n\n")
+    end
+
+    print(io, bytestring(io2))
+end
+
 
 #end of module
 end
@@ -48,11 +80,26 @@ using CellularAutomata
 
 #init=int(zeros(1000))
 #init[int(1000/2)]=1
-ca = CellularAutomaton(90, 1000)
-
-tic()
-ca = CellularAutomaton(90, 15)
-toc()
-
+#ca = CellularAutomaton(90, 1000)
+#tic()
+#ca = CellularAutomaton(90, 15)
+#toc()
 #@profile CellularAutomaton(90, 1000)
 #Profile.print()
+
+#turbine pattern
+init = int(zeros(20,20))
+init[4, 4:9] = 1
+init[5, 4:9] = 1
+
+init[4:9, 11] = 1
+init[4:9, 12] = 1
+
+init[11, 7:12] = 1
+init[12, 7:12] = 1
+
+init[7:12, 4] = 1
+init[7:12, 5] = 1
+
+#game of life
+ca = CA2d([3], [2,3], init, 5)
